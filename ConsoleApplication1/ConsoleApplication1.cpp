569363e60bd74e61cef6893c8166124967bc36a1
FinkaNKVD <lodkavovan@gmail.com>
2025-10-23 13:05:57 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+
+int main(){
+double num1, num2, max_num, chislo ;
+
+     std::cout << "Enter the first number: ";   //вводим первое число
+     std::cin >> num1;
+
+     std::cout << "Enter the second number: ";  //вводим второе число 
+     std::cin >> num2;
+
+     if (num1 > num2) {
+         max_num = num1;
+     }
+     else {
+         max_num = num2;
+     }
+
+     chislo = -sin(max_num - 5);
+
+     std::cout << "High number:" << max_num << std::endl; 
+     std::cout << "Otvet:" << chislo ;
+
+     return 0;
+
+}
+
